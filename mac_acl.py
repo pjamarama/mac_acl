4747c7dcb920c44ac90841f2b0961a7a9ecd8de0
pjamarama <40669164+pjamarama@users.noreply.github.com>
2018-08-06 18:13:39 +0300

    Update mac_acl.py
    start string number removed

--- mac_acl.py
+++ mac_acl.py
@@ -1,14 +1,8 @@
-ch1n5k1 = {
-    ip = ''
-    username = ''
-}
-
 
 numdev = int(input('How many devices? '))
 nummac = int(input('How many MAC addresses per device? '))
 
-# Number of next string
-start_string = 1000
+
 quad0 = ' 0000.0000.0000'
 
 
@@ -31,17 +25,17 @@ print (devlist)
 
 
 def output_d2m1():
-    command1 = (str(start_string+10) + ' remark N1:' + devlist[0]
+    command1 = ('remark N1:' + devlist[0]
     + ', N2:' + devlist[1])
-    command2 = (str(start_string+20) + ' permit ' + macdict.get('dev1mac1')
+    command2 = ('permit ' + macdict.get('dev1mac1')
     + quad0 + ' ' + macdict.get('dev2mac1') + quad0 + ' 0x806')
-    command3 = (str(start_string+30) + ' permit ' + macdict.get('dev2mac1')
+    command3 = ('permit ' + macdict.get('dev2mac1')
     + quad0 + ' ' + macdict.get('dev1mac1') + quad0 + ' 0x806')
-    command4 = (str(start_string+20) + ' permit ' + macdict.get('dev1mac1')
+    command4 = ('permit ' + macdict.get('dev1mac1')
     + quad0 + ' ' + macdict.get('dev2mac1') + quad0 + ' ip')
-    command5 = (str(start_string+30) + ' permit ' + macdict.get('dev2mac1')
+    command5 = ('permit ' + macdict.get('dev2mac1')
     + quad0 + ' ' + macdict.get('dev1mac1') + quad0 + ' ip')
-    return [command1, command2, command3, command4, command5]
-
-cmd_d2m1 = output_d2m1()
-print (cmd_d2m1)
+    result = [command1, command2, command3, command4, command5]
+    for a in result:
+        print(a)
+    return result
